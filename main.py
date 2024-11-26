dca7a964fdcfd97b4fd6e7c992e6148088d0f4eb
engineer5330 <nikttabusorov899@gmail.com>
2024-11-26 21:34:20 +1000

    create start main.py, create files: libs and server/ adding .gitignoge

---
+++ main.py
@@ -0,0 +1,27 @@
+token = open("TOKEN.txt").read()
+
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters.command import Command
+
+
+bot = Bot(token=token)
+dp = Dispatcher()
+
+
+@dp.message(Command("start"))
+async def cmd_start(message: types.Message):
+    await message.answer("Hello!")
+
+
+async def main():
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print("Exit")
